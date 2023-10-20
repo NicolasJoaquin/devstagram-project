b01d21421adc256a3444d9a1e66921596f099878
Nicolas Diorio <nicolas.joaquin.diorio@gmail.com>
2023-10-19 23:40:05 -0300

    Posts view+controller+routing+dropzone (prov)

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.app')
+
+@section('title')
+    Creá una nueva publicación
+@endsection
+
+@push('styles')
+    <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
+@endpush
+
+@section('content')
+    <div class="md:flex md:items-center">
+        <div class="md:w-1/2 md:m-5">
+            <form action="{{ route('images.store') }}" method="POST" enctype="multipart/form-data" id="dropzone" class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center items-center">
+
+            </form>
+        </div>    
+        <div class="md:w-1/2 p-10 bg-white rounded-lg shadow-xl mt-10 md:mt-0 md:m-5">
+            <form action="{{ route('register') }}" method="POST">
+                {{-- Directiva que me pega un input hidden con un token para evitar ataques CSR --}}
+                @csrf
+                <div class="mb-5">
+                    <label for="title" class="mb-2 block uppercase text-gray-500 font-bold">Título</label>
+                    <input
+                        id="title"
+                        name="title"
+                        type="text"
+                        placeholder="Título de la publicación"
+                        class="border p-3 w-full rounded-lg @error('title') border-red-500 @enderror"
+                        value="{{ old('title') }}"
+                    />
+                    @error('title') 
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+                <div class="mb-5">
+                    <label for="description" class="mb-2 block uppercase text-gray-500 font-bold">Descripción</label>
+                    <textarea
+                        id="description"
+                        name="description"
+                        placeholder="Descripción de la publicación"
+                        class="border p-3 w-full rounded-lg @error('description') border-red-500 @enderror"
+                    >{{ old('description') }}</textarea>
+                    @error('description') 
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+                <input
+                    type="submit"
+                    value="Crear publicación"
+                    class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
+                />
+            </form>
+        </div>    
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ImageController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
 use Illuminate\Support\Facades\Route;
@@ -34,3 +35,6 @@ Route::post('/login', [LoginController::class, 'store'])->name('login');
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
 Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+
+Route::post('/images', [ImageController::class, 'store'])->name('images.store');
