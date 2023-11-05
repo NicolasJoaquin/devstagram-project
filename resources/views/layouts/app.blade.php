1df9f101e229bd5e094de66d57e1cbebe6217543
Nicolas Diorio <nicolas.joaquin.diorio@gmail.com>
2023-11-04 21:32:46 -0300

    Update using Livewire components for dynamic liking

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
         <title>Devstagram - @yield('title')</title>
         @vite('resources/css/app.css')
         @vite('resources/js/app.js')
+        @livewireStyles
     </head>
     <body  class="bg-gray-100">
         {{-- <nav>
@@ -72,7 +73,7 @@
         <footer class="text-center p-5 text-gray-500 font-bold uppercase mt-10">
             {{-- DevStagram - Todos los derechos reservados {{date('Y')}} --}}
             DevStagram - Todos los derechos reservados {{ now()->year }}
-
         </footer>
+        @livewireScripts
     </body>
 </html>
